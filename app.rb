07313d660e97b4a6f160840b5b74ee09408389b6
abdal90 <aabdalla3@mail.greenriver.edu>
2019-01-20 19:08:58 +0000

    commit before deploy section

---
+++ app.rb
@@ -0,0 +1,8 @@
+
+require 'sinatra'
+
+class MyApp < Sinatra::Base
+  get '/' do
+    '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Hello World</title></head><body><h1>Goodbye World</h1></body></html>'
+  end
+end
